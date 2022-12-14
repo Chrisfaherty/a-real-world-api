eb7505772ab6f331509a2fc912948e38d65b3209
Chrisfaherty <chrismactom@gmail.com>
2022-12-14 16:07:45 +0000

    Create the const's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-khschNFGuM0qy8hD3IBvH3B4BtU
+const API_KEY = "khschNFGuM0qy8hD3IBvH3B4BtU";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
